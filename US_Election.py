0a0e34c0cf81149966fb8f966fac1650bf037a53
Laniakea <marie.dupont@gmail.com>
2020-03-25 17:48:09 +0100

    Pourcentage

--- US_Election.py
+++ US_Election.py
@@ -1,9 +1,20 @@
+
+
+"""Hypothèses : 
+    Chaque Etat a au moins 1 représentant puis 3
+    On prend en compte la population totale de l'Etat (pour l'instant et on comparera avec seulement la population de votants)
+    On ne prend pas en compte Puerto Rico, îles Marianne... car pas de data (éventuellement faire les 2 cas hypothèses 100% Trump et 100% CLinton et peut être un 50-50)
+    
+    """
+    
+# 101 contraintes
+    
 import pulp
 import pandas as pd
 
-N_votes = 538  # nombre de grands électeurs
+N_votes = 538 # nombre de grands électeurs
 # extraction des données depuis le fichier .csv
-data = pd.read_csv('US_voters_without_PuertoRico.csv')  # données du site census.gov, citoyens de + de 18 ans
+data = pd.read_csv('US_voters.csv', sep='\t')  # données du site census.gov, citoyens de + de 18 ans
 
 # définition du problème
 prob = pulp.LpProblem("répartition des grands électeurs", pulp.LpMinimize)
@@ -14,7 +25,7 @@ v = pulp.LpVariable('v', 0, None, pulp.LpContinuous)
 alpha_list = []  # la variable 'alpha' représente le nombre de grands électeurs dans l'Etat 'i'
 population_list = []  # population dans l'Etat 'i'
 for i in range(data.__len__()):
-    alpha_i = pulp.LpVariable(str(data.iloc[i, 0]), 3, None, pulp.LpInteger)  # définition des variables alpha_i
+    alpha_i = pulp.LpVariable(str(data.iloc[i, 0]), 0, None, pulp.LpInteger)  # définition des variables alpha_i   # [i,0]Va chercher le nom de l'etat
     alpha_list.append(alpha_i)
     population_list.append(int(data.iloc[i, 1]))
 
@@ -31,23 +42,33 @@ prob += sum(alpha_list) == N_votes
 prob.solve()
 print("status : ", pulp.LpStatus[prob.status])
 
+
 # affichage des résultats
 states = [v.name for v in prob.variables()]
 nb_electors = [v.varValue for v in prob.variables()]
-states = states[:-2]  # on enlève les deux dernières variables qui sont u et v car elles ne nous intéressent pas
+states = states[:-2]
 nb_electors = nb_electors[:-2]
-df = pd.DataFrame(nb_electors, index=states)  # création d'une DataFrame Pandas juste pour le format d'affichage
+df = pd.DataFrame(nb_electors, index=states)
 print(df)
 
 # addition des votes (pour D. Trump)
-s = 0
+#s = 0
+sTrump = 0
+sHillary = 0
 for i in range(data.__len__()):
-    s += data.iloc[i, 2] * nb_electors[i]
+    #s += data.iloc[i, 2] * nb_electors[i]
+    sTrump += int(data.iloc[i, 3] * nb_electors[i])
+    sHillary += int(data.iloc[i, 4] * nb_electors[i])
+    
 
 # Résultats des votes
-if s > N_votes / 2:
-    print('Donald Trump wins ! with : ', s, ' votes')
-elif s < N_votes / 2:
-    print('Hillary Clinton wins ! with : ', N_votes - s, ' votes')
+if sTrump > sHillary:
+    print('Donald Trump wins ! with : ', sTrump, ' votes')
+    print('Hillary gots : ', sHillary, 'votes')
+    print('Total number of votes : ', sTrump + sHillary)
+elif sTrump < sHillary:
+    print('Hillary Clinton wins ! with : ', sHillary, ' votes')
+    print('Trump gots : ', sTrump, 'votes')
+    print('Total number of votes : ', sTrump + sHillary)
 else:
     print("it's a draw !")
